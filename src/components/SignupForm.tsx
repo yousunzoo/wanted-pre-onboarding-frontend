72862c80b2f1208cad0e05cb96eee58c81405e19
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-05 01:36:43 +0900

    Feat: 회원가입 폼 유효성 검증 유효성에 맞지 않을 시 에러 메시지 출력 및 회원가입 버튼 비활성화

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,22 +1,44 @@
+import { ChangeEvent, useState } from 'react'
 import {MdOutlineMail, MdOutlineLock} from 'react-icons/md'
+
+const DIV_STYLE = 'mb-8 flex flex-col gap-4'
+const LABEL_STYLE = 'text-lg inline-flex items-center gap-1'
+const INPUT_STYLE = 'rounded-3xl border-2 px-6 py-2 focus:outline-slate-600'
+const TEXT_STYLE ='text-gray-500 px-2'
+
 function SignupForm() {
+  const [info, setInfo] = useState({email:'', password:''})
+  const [isValidate, setIsValidate] = useState({email:false, password:false})
+
+  const onChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const {id,value} = e.target;
+    setInfo({...info, [id]:value})
+    if(id === 'email'){
+      setIsValidate({...isValidate, email: value.includes('@') ? true : false})
+    }
+    if(id === 'password'){
+      setIsValidate({...isValidate, password: value.length >= 8 ? true :false})
+    }
+  }
+
+  const isDisabled = info.email && info.password && isValidate.email && isValidate.password ? false : true
   return (
     <form className='w-full px-10'>
-      <div className='mb-8 flex flex-col gap-4'>
-        <label className='text-lg inline-flex items-center gap-1' htmlFor='email'>
+      <div className={DIV_STYLE}>
+        <label className={LABEL_STYLE} htmlFor='email'>
           <MdOutlineMail className='text-2xl' /> 이메일</label>
-        <input className='rounded-3xl border-2 px-6 py-2 focus:outline-slate-600' type='email' data-testid='email-input' id='email' placeholder='test@test.com' />
-        <p className='text-gray-500 px-2'>이메일 형식에 맞게 입력해주세요.</p>
+        <input className={INPUT_STYLE} type='email' data-testid='email-input' id='email' onChange={onChange} value={info.email} placeholder='test@test.com' />
+        <p className={`${TEXT_STYLE} ${info.email && !isValidate.email && 'text-red-500'}`}>이메일 형식에 맞게 입력해주세요.</p>
       </div>
-      <div className='mb-10 flex flex-col gap-4'>
-        <label className='text-lg inline-flex items-center gap-1' htmlFor='password'>
+      <div className={DIV_STYLE}>
+        <label className={LABEL_STYLE} htmlFor='password'>
           <MdOutlineLock className='text-2xl' /> 비밀번호</label>
-        <input className='rounded-3xl border-2 px-6 py-2 focus:outline-slate-600' type='password' data-testid='password-input' id='password' placeholder='비밀번호를 입력해주세요' />
-        <p className='text-gray-500 px-2'>비밀번호를 8자리 이상 입력해주세요.</p>
+        <input className={INPUT_STYLE} type='password' data-testid='password-input' id='password' onChange={onChange} value={info.password} placeholder='비밀번호를 입력해주세요' />
+        <p className={`${TEXT_STYLE} ${info.password && !isValidate.password && 'text-red-500'}`}>비밀번호를 8자리 이상 입력해주세요.</p>
       </div>
       <div className='flex w-full gap-4'>
       <button className='w-1/2 py-3 border-4 rounded-lg border-[#ffdc72] hover:bg-[#ffdc72]'>로그인하러 가기</button>
-      <button className='w-1/2 bg-[#ffdc72] hover:bg-[#ffd278] rounded-lg py-3' disabled>회원가입</button>
+      <button className={`w-1/2 rounded-lg py-3 ${isDisabled ? 'bg-gray-300' : 'bg-[#ffdc72] hover:bg-[#ffd278]'}`} disabled={isDisabled}>회원가입</button>
       </div>
     </form>
   )
