fbeb571ec5173894c4e657487c7ca8bb318c4cc5
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-05 00:33:26 +0900

    Design: Layout 컴포넌트 스타일링

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <main className='w-[600px] h-[600px] bg-white'>
+    <main className='w-[600px] h-[600px] bg-white rounded-lg p-12'>
       <Outlet />
     </main>
   )
