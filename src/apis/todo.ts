19cc63b44cde89c414ea1fa97d5257078954fb3e
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-06 01:23:19 +0900

    Feat: todo 관련 api 함수 세팅

--- src/apis/todo.ts
+++ src/apis/todo.ts
@@ -8,3 +8,37 @@ export const addTodo = async (todo: string) => {
 		return error.response;
 	}
 };
+
+interface TodoProps {
+	todo: {
+		todo: string;
+		isCompleted: boolean;
+	};
+	id: number;
+}
+
+export const updateTodo = async ({ todo, id }: TodoProps) => {
+	try {
+		const response = await axiosInstance.put(`/todos/${id}`, { ...todo });
+		return response;
+	} catch (error: any) {
+		return error.response;
+	}
+};
+export const deleteTodo = async (id: number) => {
+	try {
+		const response = await axiosInstance.delete(`/todos/${id}`);
+		return response;
+	} catch (error: any) {
+		return error.response;
+	}
+};
+
+export const getTodos = async () => {
+	try {
+		const response = await axiosInstance.get('/todos');
+		return response;
+	} catch (error: any) {
+		return error.response;
+	}
+};
