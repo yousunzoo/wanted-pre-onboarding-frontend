f87ec28753287d04c15ceebf7ba08018ccc43c88
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-07 01:31:44 +0900

    Design: 레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Router from "./routes/Router";
+import Router from './routes/Router';
 
 function App() {
-  return (
-    <div className="App flex h-[100vh] bg-[#30a2ff] justify-center items-center">
-      <Router />
-    </div>
-  );
+	return (
+		<div className='App flex flex-col h-[100vh] bg-[#30a2ff] justify-center items-center'>
+			<Router />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
-  return (
-    <main className='w-[600px] h-[600px] bg-white rounded-lg p-12'>
-      <Outlet />
-    </main>
-  )
+	return (
+		<>
+			<main className='w-[600px] h-[600px] bg-white rounded-b-xl p-12'>
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
-export default Layout
+export default Layout;
