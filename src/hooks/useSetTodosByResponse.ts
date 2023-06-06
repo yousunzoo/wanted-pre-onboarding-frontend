331c7e582b4dd1fed3b0de631604e333a5ca1d13
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-06 19:33:31 +0900

    Feat: Todo 페이지 실행 시 Todos 불러오는 훅 작성 setTodosByResponse 재사용 가능하게 만들어 새로운 할 일 추가할 때도 getTodos 실행

---
+++ src/hooks/useSetTodosByResponse.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getTodos } from '../apis/todo';
+import { Todos } from '../interface/todo';
+
+interface UseSetTodosByResponse {
+	(): [Todos | [], boolean, () => Promise<void>];
+}
+const useSetTodosByResponse: UseSetTodosByResponse = () => {
+	const [todos, setTodos] = useState<Todos>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const setTodosByResponse = async () => {
+		const { data } = await getTodos();
+		setTodos(data);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		setTodosByResponse();
+	}, []);
+
+	return [todos, isLoading, setTodosByResponse];
+};
+
+export default useSetTodosByResponse;
