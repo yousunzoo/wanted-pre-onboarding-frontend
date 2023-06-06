e0f668cc66a5ae1c9c7b44627752302842893b60
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-07 01:52:45 +0900

    Feat: Header 컴포넌트 구현

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 function Layout() {
 	return (
 		<>
+			<Header />
 			<main className='w-[600px] h-[600px] bg-white rounded-b-xl p-12'>
 				<Outlet />
 			</main>
