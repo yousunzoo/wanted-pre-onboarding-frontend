e88982bd2c551b12a441572de53e293ca902fa13
Seonju Yoo <102499959+yousunzoo@users.noreply.github.com>
2023-06-07 23:32:35 +0900

    Feat: data 속성 추가 (#16)
    * Feat: NotFound 페이지 구현
    
    * Feat: data 속성 추가

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,10 +1,10 @@
-import Form from '../components/Form';
+import AccountForm from '../components/AccountForm';
 
 function Signin() {
 	return (
 		<>
 			<h2 className='text-2xl text-center font-semibold mb-14'>로그인</h2>
-			<Form />
+			<AccountForm />
 		</>
 	);
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,10 @@
-import Form from '../components/Form';
+import AccountForm from '../components/AccountForm';
 
 function Signup() {
 	return (
 		<>
 			<h2 className='text-2xl text-center font-semibold mb-14'>회원가입</h2>
-			<Form />
+			<AccountForm />
 		</>
 	);
 }
